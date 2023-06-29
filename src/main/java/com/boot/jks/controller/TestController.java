242c962d1c9bcd89c41a66682c18b021e3ffd6cf
qinzhong.luo <qinzhong.luo@qitacn.com>
2023-06-29 14:43:35 +0800

    dd环境的

--- src/main/java/com/boot/jks/controller/TestController.java
+++ src/main/java/com/boot/jks/controller/TestController.java
@@ -10,6 +10,6 @@ public class TestController {
 
     @GetMapping("getinfo")
     public String GetInfo(){
-        return "Hello Jenkins 我是过薄弱啊啊啊啊xxxxx";
+        return "Hello Jenkins xxxxx 姐 效果啊u共啊 啊 ";
     }
 }
